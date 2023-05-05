776815fe93a39d9ec8921894547a07cefdfb2fc7
Aulia Rachmat Diapari <aulia.diapari@gmail.com>
2023-05-06 01:25:17 +0700

    commit1

--- rakamin-py/src/locators/LoginLocators.py
+++ rakamin-py/src/locators/LoginLocators.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-class LoginLocators():
+class LoginLocators:
 
     email_field = (By.NAME, 'email')
     password_field = (By.NAME, 'password')
--- rakamin-py/tests/test_login_invalid_user.py
+++ rakamin-py/tests/test_login_invalid_user.py
@@ -1,3 +1,5 @@
+import pdb
+
 import self as self
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -5,10 +7,21 @@ from selenium.webdriver.common.by import By
 
 class TestLoginInvalidUser:
     def setup_method(self, method):
-        self.driver = webdriver.Chrome()
-        method = vars()
+        self.driver = webdriver.Edge()
 
-    def test_login_invalid_user_go_homepage(self, driver):
-        self.driver.maximize_window()
+    def test_login_invalid_user_go_homepage(self):
         self.driver.get('https://jubelio.com')
+        self.driver.find_element(By.XPATH,
+                                 '/html/body/div[1]/section[1]/div/div[3]/div/div[2]/div/div/a/span/span').click()
+        print(self.driver.current_window_handle.title())
+        all_window_handles = self.driver.window_handles
+        default_window = all_window_handles[0]
+        new_window = all_window_handles[1]
+        self.driver.switch_to.window(new_window)
+        print(new_window.title())
+        self.driver.find_element(By.NAME, 'email').send_keys('dummymail@gmail.com')
+        self.driver.find_element(By.NAME, 'password').send_keys('dummypass')
+        self.driver.find_element(By.XPATH, '//*[@id="shell"]/div/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div['
+                                           '2]/form/div[4]/button').click()
 
+        pdb.set_trace()
