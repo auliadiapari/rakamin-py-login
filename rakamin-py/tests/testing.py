8faf119162a0393bc47b0450bececa7dc8d53521
Aulia Rachmat Diapari <aulia.diapari@gmail.com>
2023-05-05 22:22:36 +0700

    commit1

---
+++ rakamin-py/tests/testing.py
@@ -0,0 +1,14 @@
+import pdb
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Edge()
+
+driver.get('https://jubelio.com')
+# title = driver.title
+# assert "Jubelio - Solusi Omnichannel Untuk Pebisnis Online & Offline" in title
+# print("Assertion is Passed")
+
+
+pdb.set_trace()
